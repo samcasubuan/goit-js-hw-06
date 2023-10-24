04b6ed230679ddcdff6f64d36b9eb053f0434468
samcasubuan <s.mcasubuan23@yahoo.com>
2023-10-24 19:59:30 +0800

    task 1 update

--- js/task-01.js
+++ js/task-01.js
@@ -4,4 +4,8 @@ console.log("number of categories:", categoriesElements.length);
 categoriesElements.forEach((el) => {
   const categoriesElements = el.firstElementChild.textContent;
   console.log("category:", categoryName);
+
+  const categoryTypesList = el.lastElementChild;
+  const categoryTypesList = categoryTypesList.childElementCount.length;
+  console.log("Elements:", categoryTypesLength);
 });
