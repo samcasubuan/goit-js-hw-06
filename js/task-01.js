d4ececf6ff6ce12d42db3bc78ca4ca8387bc9ba4
samcasubuan <136684708+samcasubuan@users.noreply.github.com>
2023-10-24 19:53:57 +0800

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,12 @@
+const categoriesElements = document.querySelectorAll(".item");
+console.log("number of categories:", categoriesElements.length);
+
+categoriesElements.forEach((el) => {
+  const categoryName = el.firstElementChild.textContent;
+  console.log("category:", categoryName);
+
+  const categoryTypeList = el.lastElementChild;
+  const categoryTypesLenght = categoryTypeList.children.length;
+  console.log("Elements:", categoryTypesLenght);
+});
 
