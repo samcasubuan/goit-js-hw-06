59b46b0bd71d9139f35b6703d8bf78044a438b6d
samcasubuan <s.mcasubuan23@yahoo.com>
2023-10-24 20:07:38 +0800

    task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,18 +1,11 @@
 const categoriesElements = document.querySelectorAll(".item");
-console.log("number of categories:", categoriesElements.length);
+console.log("Number of categories:", categoriesElements.length);
 
 categoriesElements.forEach((el) => {
   const categoryName = el.firstElementChild.textContent;
-  console.log("category:", categoryName);
+  console.log("Category:", categoryName);
 
-<<<<<<< HEAD
   const categoryTypesList = el.lastElementChild;
-  const categoryTypesList = categoryTypesList.childElementCount.length;
+  const categoryTypesLength = categoryTypesList.children.length;
   console.log("Elements:", categoryTypesLength);
-=======
-  const categoryTypeList = el.lastElementChild;
-  const categoryTypesLenght = categoryTypeList.children.length;
-  console.log("Elements:", categoryTypesLenght);
->>>>>>> b52d7912f1e7139b5f034198046c3324c840ebf6
 });
-
