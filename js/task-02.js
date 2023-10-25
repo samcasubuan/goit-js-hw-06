66e577b60ddd6477ee72a2f22596d44093cab3a5
samcasubuan <s.mcasubuan23@yahoo.com>
2023-10-25 20:01:34 +0800

    new task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const ingredientsArr = ingredients.map((ingredient) => {
+  const ingredientItem = document.createElement("Li");
+  ingredientItem.classList.add("item");
+  ingredientItem.textContent = ingredient;
+
+  return ingredientItem;
+});
+
+ingredientsList.append(...ingredientsArr);
