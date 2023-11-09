a303ee056f3a69508b41016c39d19c9f7eb577fb
samcasubuan <s.mcasubuan23@yahoo.com>
2023-11-09 19:16:40 +0800

    new

--- js/task-04.js
+++ js/task-04.js
@@ -1,28 +1,15 @@
-// const decrementEl = document.querySelector('[data-action="decrement"]');
-// const incrementEl = document.querySelector('[data-action="increment"]');
-// const counterValue = document.querySelector("#value");
+const decrementEl = document.querySelector('[data-action="decrement"]');
+const incrementEl = document.querySelector('[data-action="increment"]');
+const counterValue = document.querySelector("#value");
 
-const increase = document.querySelector(
-  '#counter button[data-action="increment"]'
-);
-const decrease = document.querySelector(
-  '#counter button[data-action="decrement"]'
-);
-const value = document.querySelector("#value");
-let counterValue = 0;
+let value = 0;
 
-function counterChange() {
-  const up = increase.dataset.action;
-  const down = decrease.dataset.action;
-  if (this.dataset.action === up) {
-    counterValue += 1;
-    return (value.innerHTML = counterValue);
-  }
-  if (this.dataset.action === down) {
-    counterValue -= 1;
-    return (value.innerHTML = counterValue);
-  }
-}
+decrementEl.addEventListener("click", function () {
+  value--;
+  counterValue.textContent = value;
+});
 
-increase.addEventListener("click", counterChange);
-decrease.addEventListener("click", counterChange);
+incrementEl.addEventListener("click", function () {
+  value++;
+  counterValue.textContent = value;
+});
